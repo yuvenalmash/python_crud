b5420f737a4aa8bdab4c31c1132b893b8df761ef
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-09-05 23:57:07 +0300

    Create config file

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+# Description: Configuration file for the application
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/python_crud'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
