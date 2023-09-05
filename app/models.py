8687201401268c84ca482cfe8157681e1b387a18
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-09-05 23:55:39 +0300

    Create model

---
+++ app/models.py
@@ -0,0 +1,9 @@
+# app/models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
