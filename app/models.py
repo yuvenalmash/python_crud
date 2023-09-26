bd6a6eff94db21b919a8d4da95b035ed2fa03c6b
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-09-26 22:40:44 +0300

    Remove unused files

--- app/models.py
+++
@@ -1,9 +0,0 @@
-# app/models.py
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200))
--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-# Description: Configuration file for the application
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/python_crud'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
